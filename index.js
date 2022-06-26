92a230fe2cba8a41c216d0e0192b8712727e8ad1
devansh <dunit99k@gmail.com>
2022-06-27 01:45:36 +0530

    cors

--- index.js
+++ index.js
@@ -15,8 +15,17 @@ dotenv.config();
 app.use(express.json());
 app.use(cors({ 
   origin: "https://devansh-walia.github.io/mern_blogeasy/", 
-  credentials: true 
- }));
+  credentials: true ,
+  optionSuccessStatus:200
+}));
+
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose
